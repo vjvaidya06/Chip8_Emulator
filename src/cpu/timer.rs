4587f760a31673b3350526016519869d25712bf3
Vivek Vaidya <vjvaidya2006@gmail.com>
2026-08-29 16:30:55 -0400

    Add fixed timer struct, since I forgot to before

--- src/cpu/timer.rs
+++ src/cpu/timer.rs
@@ -1,7 +1,7 @@
 use std::time::Instant;
 
 pub(super) struct Chip8Timer{
-    start_time: Option<Instant>,
+    last_check: Option<Instant>,
     //In seconds
     pub duration: f64
 }
@@ -9,23 +9,34 @@ pub(super) struct Chip8Timer{
 impl Chip8Timer{
     pub(super) fn new(start: Option<Instant>, length: f64) -> Self{
         Chip8Timer{
-            start_time: start,
+            last_check: start,
             duration: length
         }
     }
     pub(super) fn set_duration(&mut self, new_duration: f64){
-        self.start_time = Some(Instant::now());
-        self.duration = new_duration;
+        self.last_check = Some(Instant::now());
+        //Stored in ticks, convert to seconds
+        self.duration = new_duration/60.0;
         // println!("Timer set to duration: {new_duration}");
     }
     //Returns true if timer has elapsed
+    //TODO: timer does not store the delay properly
     pub(super) fn update(&mut self) -> bool{
-        if let Some(time) = self.start_time{
+        if let Some(time) = self.last_check{
             let time_passed = time.elapsed().as_secs_f64();
-            if time_passed > 1.0/60.0 && time_passed >= self.duration{
-                self.start_time = None;
-                return true;
+            if time_passed > 1.0/60.0{
+                self.duration -= time_passed;
+                self.last_check = Some(Instant::now());
+                if self.duration <= 0.0{
+                    self.duration = 0.0;
+                    self.last_check = None;
+                    return true;
+                }
             }
+            // if time_passed > 1.0/60.0 && time_passed >= self.duration{
+            //     self.last_check = None;
+            //     return true;
+            // }
         }
         false
     }
